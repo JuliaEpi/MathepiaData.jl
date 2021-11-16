077d29f518492f04f15d53815ee85aba73f4ad79
Documenter.jl <documenter@juliadocs.github.io>
2021-11-16 20:14:06 +0000

    build based on 325f719

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MathepiaData","category":"page"},{"location":"#MathepiaData","page":"Home","title":"MathepiaData","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MathepiaData.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MathepiaData]","category":"page"}]
+}
